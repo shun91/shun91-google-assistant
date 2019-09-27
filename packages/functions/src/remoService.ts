cb99ac8e0fc0ed1ac3ea5236ade1d52ef3a4375c
shun91 <shun9167@gmail.com>
2019-09-27 22:43:14 +0900

    ♻ get -> fetch に変更

--- packages/functions/src/remoService.ts
+++ packages/functions/src/remoService.ts
@@ -4,7 +4,7 @@ import credentials from '../../../credentials.json';
 // gateways
 // -----------------------------------------------------------------------------
 const REMO_TOKEN = credentials.remoToken;
-const getDevices = async () =>
+const fetchDevices = async () =>
   get('https://api.nature.global/1/devices', {
     headers: { Authorization: `Bearer ${REMO_TOKEN}` },
     json: true,
@@ -13,19 +13,19 @@ const getDevices = async () =>
 // services
 // -----------------------------------------------------------------------------
 export const getTemperatureText = async () => {
-  const devices = await getDevices();
+  const devices = await fetchDevices();
   return `現在の室温は${devices[0].newest_events.te.val.toFixed(1)}度です`;
 };
 
 export const getHumidityText = async () => {
-  const devices = await getDevices();
+  const devices = await fetchDevices();
   return `現在の湿度は${devices[0].newest_events.hu.val.toFixed(
     1,
   )}パーセントです`;
 };
 
 export const getTemperatureAndHumidityText = async () => {
-  const devices = await getDevices();
+  const devices = await fetchDevices();
   const temperature = devices[0].newest_events.te.val.toFixed(1);
   const humidity = devices[0].newest_events.hu.val.toFixed(1);
   return `現在の室温は${temperature}度、湿度は${humidity}パーセントです`;
