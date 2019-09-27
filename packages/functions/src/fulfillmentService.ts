ae92b30007be0ccfab1b091f207e1c710b423e0e
shun91 <shun9167@gmail.com>
2019-09-27 22:39:04 +0900

    ♻ gateway と service に分離

--- packages/functions/src/fulfillmentService.ts
+++ packages/functions/src/fulfillmentService.ts
@@ -1,12 +1,19 @@
 import { get } from 'request-promise';
 import credentials from '../../../credentials.json';
 
+// gateways
+// -----------------------------------------------------------------------------
+const fetchFulfillment = async () => get(credentials.fulfillmentUrl);
+
+// services
+// -----------------------------------------------------------------------------
+
 /**
  * fulfillment のエンドポイントにリクエストする
  */
 export const wakeUp = async () => {
   console.info('start');
-  const res = await get(credentials.fulfillmentUrl).catch(
+  const res = await fetchFulfillment().catch(
     e => e.message || 'Request error but wake up is successful',
   );
   console.info('[response]', res);
