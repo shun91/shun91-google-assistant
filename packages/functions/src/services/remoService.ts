a13603a170319fb229cc3eb1f3aaefbfe57eeb9b
shun91 <shun9167@gmail.com>
2019-10-01 17:06:18 +0900

    👍 温度・湿度のtoFixedを削除
    しなくても十分発話可能

--- packages/functions/src/services/remoService.ts
+++ packages/functions/src/services/remoService.ts
@@ -14,19 +14,17 @@ const fetchDevices = async () =>
 // -----------------------------------------------------------------------------
 export const getTemperatureText = async () => {
   const devices = await fetchDevices();
-  return `現在の室温は${devices[0].newest_events.te.val.toFixed(1)}度です`;
+  return `現在の室温は${devices[0].newest_events.te.val}度です`;
 };
 
 export const getHumidityText = async () => {
   const devices = await fetchDevices();
-  return `現在の湿度は${devices[0].newest_events.hu.val.toFixed(
-    1,
-  )}パーセントです`;
+  return `現在の湿度は${devices[0].newest_events.hu.val}パーセントです`;
 };
 
 export const getTemperatureAndHumidityText = async () => {
   const devices = await fetchDevices();
-  const temperature = devices[0].newest_events.te.val.toFixed(1);
-  const humidity = devices[0].newest_events.hu.val.toFixed(1);
+  const temperature = devices[0].newest_events.te.val;
+  const humidity = devices[0].newest_events.hu.val;
   return `現在の室温は${temperature}度、湿度は${humidity}パーセントです`;
 };
