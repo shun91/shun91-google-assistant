2a7ce2fee5ec47c8087d7d14963233b320128562
shun91 <shun9167@gmail.com>
2019-10-02 21:13:40 +0900

    🐛 温度は四捨五入する必要があった...

--- packages/functions/src/services/remoService.ts
+++ packages/functions/src/services/remoService.ts
@@ -14,7 +14,7 @@ const fetchDevices = async () =>
 // -----------------------------------------------------------------------------
 export const getTemperatureText = async () => {
   const devices = await fetchDevices();
-  return `現在の室温は${devices[0].newest_events.te.val}度です`;
+  return `現在の室温は${devices[0].newest_events.te.val.toFixed(1)}度です`;
 };
 
 export const getHumidityText = async () => {
@@ -24,7 +24,7 @@ export const getHumidityText = async () => {
 
 export const getTemperatureAndHumidityText = async () => {
   const devices = await fetchDevices();
-  const temperature = devices[0].newest_events.te.val;
+  const temperature = devices[0].newest_events.te.val.toFixed(1);
   const humidity = devices[0].newest_events.hu.val;
   return `現在の室温は${temperature}度、湿度は${humidity}パーセントです`;
 };
